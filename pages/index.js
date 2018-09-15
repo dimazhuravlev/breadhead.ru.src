384d4767c6e679c5b03580404488a918ce74a91b
Igor Kamyshev <igor@kamyshev.me>
2018-09-15 11:57:19 +0300

    Added linters and some config

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Welcome to Sold Soda!</div>
+import styles from './index.css'
+
+import Button from '@site/ui/atoms/Button'
+
+export default () => (
+  <div>
+    <div className={styles.root}>Welcome to Sold Soda!</div>
+    <Button />
+  </div>
+)
---
+++ ui/atoms/Button/index.js
@@ -0,0 +1,3 @@
+import styles from './Button.css'
+
+export default () => <button className={styles.button}>Welcome to Sold Soda!</button>
