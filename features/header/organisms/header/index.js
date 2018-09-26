2e5baf3277dd557b84c33a9f93027f6e054b6e19
Mac <dmitryzhur@gmail.com>
2018-09-26 15:44:06 +0300

    removed button from header

--- features/header/organisms/header/index.js
+++ features/header/organisms/header/index.js
@@ -2,17 +2,13 @@ import React from 'react'
 
 import Logo from '../../atoms/logo'
 import Navigation from '../../molecules/navigation'
-import Button from '@site/ui/atoms/Button'
 import * as styles from './header.css'
 
 const Header = () => {
   return (
     <div className={styles.Header}>
-      <div className={styles.HeaderWrapper}>
-        <Logo />
-        <Navigation />
-      </div>
-      <Button />
+      <Logo />
+      <Navigation />
     </div>
   )
 }
