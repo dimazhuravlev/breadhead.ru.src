f7ea55bd59485f011d27dbf1c823f4422b8cd583
Mac <dmitryzhur@gmail.com>
2018-09-27 12:50:06 +0300

    added PlusIcon

--- ui/molecules/Icons/PlusIcon.js
+++ ui/molecules/Icons/PlusIcon.js
@@ -1,19 +1,26 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <!-- Generator: Sketch 50.2 (55047) - http://www.bohemiancoding.com/sketch -->
-    <title>icon</title>
-    <desc>Created with Sketch.</desc>
-    <defs></defs>
-    <g id="OK" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="bh-site-2018_desktop-1440" transform="translate(-402.000000, -2696.000000)" fill="#0E0F0F">
-            <g id="btn_more" transform="translate(382.000000, 2688.000000)">
-                <g id="icon">
-                    <g transform="translate(20.000000, 8.000000)">
-                        <rect id="Rectangle-2" x="15" y="0" width="2" height="32"></rect>
-                        <rect id="Rectangle-5" x="0" y="15" width="32" height="2"></rect>
-                    </g>
-                </g>
+import React from 'react'
+
+const PlusIcon = () => (
+  <svg
+    width="32px"
+    height="32px"
+    viewBox="0 0 32 32"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g transform="translate(-402.000000, -2696.000000)" fill="#0E0F0F">
+        <g transform="translate(382.000000, 2688.000000)">
+          <g>
+            <g transform="translate(20.000000, 8.000000)">
+              <rect id="Rectangle-2" x="15" y="0" width="2" height="32" />
+              <rect id="Rectangle-5" x="0" y="15" width="32" height="2" />
             </g>
+          </g>
         </g>
+      </g>
     </g>
-</svg>
+  </svg>
+)
+
+export default PlusIcon
