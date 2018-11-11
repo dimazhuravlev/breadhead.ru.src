5902646fb86db4cfb1b0ac176df5c4671390db57
uncleseneca <uncleseneca@gmail.com>
2018-11-11 15:14:17 +0300

    plugged in bar

--- features/slickSlider/organisms/Slider/index.js
+++ features/slickSlider/organisms/Slider/index.js
@@ -1,21 +1,63 @@
 import React from 'react'
 import SlickSlider from 'react-slick'
+import VisibilitySensor from 'react-visibility-sensor'
 import styles from './Slider.css'
 import './SliderGlobal.css?CSSModulesDisable'
 import NavButton from '../../molecules/NavButton'
 import Bar from '../../molecules/Bar'
+
 const settings = {
   prevArrow: <NavButton className={styles.navButton} direction="left" />,
   nextArrow: <NavButton className={styles.navButton} direction="right" />,
 }
 class Slider extends React.Component {
+  state = { index: 0 }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { index: prevIndex } = prevState
+    const { index } = this.state
+
+    if (prevIndex !== index) {
+      this.setState({ index })
+    }
+  }
+
+  beforeChange = (_, index) => {
+    this.setState(() => ({ index }))
+  }
+
+  sliderRef = React.createRef()
+
   render() {
     const { children } = this.props
+    const { index } = this.state
+
     return (
-      <div className={styles.wrapper}>
-        <Bar index={0} quantity={children.length} onRest={() => {}} />
-        <SlickSlider {...settings}>{children}</SlickSlider>
-      </div>
+      <VisibilitySensor partialVisibility>
+        {({ isVisible }) => (
+          <div className={styles.wrapper}>
+            <Bar
+              index={index}
+              quantity={children.length}
+              onRest={arg => {
+                // console.log(arg)
+                debugger
+                this.sliderRef.slickNext()
+              }}
+            />
+            <SlickSlider
+              ref={slider => (this.sliderRef = slider)}
+              beforeChange={this.beforeChange}
+              {...settings}
+            >
+              {children}
+            </SlickSlider>
+            <span className={styles.sensor}>
+              {isVisible ? 'visible' : 'invisible'}
+            </span>
+          </div>
+        )}
+      </VisibilitySensor>
     )
   }
 }
