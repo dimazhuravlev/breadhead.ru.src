35c1094099f1e1351d27c37f8a375fc7fe39623f
uncleseneca <uncleseneca@gmail.com>
2018-11-13 12:01:23 +0300

    fixed linter issues

--- features/slider/molecules/Bar/atoms/TimeLine/index.js
+++ features/slider/molecules/Bar/atoms/TimeLine/index.js
@@ -17,58 +17,58 @@ class TimeLine extends React.Component {
   render() {
     const { state, onRest } = this.props
     switch (state) {
-      case 'frozen':
-        return (
-          <div className={styles.timeLine}>
-            <div
-              style={{ transform: `scaleX(${this.transform})` }}
-              className={styles.progressBar}
-            />
-          </div>
-        )
-      case 'active':
-        return (
-          <Spring
-            reset={this.state.reset}
-            onRest={onRest}
-            onFrame={({ transform }) => (this.transform = transform)}
-            config={{ duration: SLIDE_DELAY - SLIDE_DELAY * this.transform }}
-            from={{ transform: this.transform || 0 }}
-            to={{ transform: 1 }}
-          >
-            {({ transform }) => {
-              return (
-                <div className={styles.timeLine}>
-                  <div
-                    style={{ transform: `scaleX(${transform})` }}
-                    className={styles.progressBar}
-                  />
-                </div>
-              )
-            }}
-          </Spring>
-        )
-      case 'empty':
-        return (
-          <div className={styles.timeLine}>
-            <div
-              style={{ transform: `scaleX(${0})` }}
-              className={styles.progressBar}
-            />
-          </div>
-        )
-      case 'full':
-        return (
-          <div className={styles.timeLine}>
-            <div
-              style={{ transform: `scaleX(${1})` }}
-              className={styles.progressBar}
-            />
-          </div>
-        )
+    case 'frozen':
+      return (
+        <div className={styles.timeLine}>
+          <div
+            style={{ transform: `scaleX(${this.transform})` }}
+            className={styles.progressBar}
+          />
+        </div>
+      )
+    case 'active':
+      return (
+        <Spring
+          reset={this.state.reset}
+          onRest={onRest}
+          onFrame={({ transform }) => (this.transform = transform)}
+          config={{ duration: SLIDE_DELAY - SLIDE_DELAY * this.transform }}
+          from={{ transform: this.transform || 0 }}
+          to={{ transform: 1 }}
+        >
+          {({ transform }) => {
+            return (
+              <div className={styles.timeLine}>
+                <div
+                  style={{ transform: `scaleX(${transform})` }}
+                  className={styles.progressBar}
+                />
+              </div>
+            )
+          }}
+        </Spring>
+      )
+    case 'empty':
+      return (
+        <div className={styles.timeLine}>
+          <div
+            style={{ transform: `scaleX(${0})` }}
+            className={styles.progressBar}
+          />
+        </div>
+      )
+    case 'full':
+      return (
+        <div className={styles.timeLine}>
+          <div
+            style={{ transform: `scaleX(${1})` }}
+            className={styles.progressBar}
+          />
+        </div>
+      )
 
-      default:
-        return null
+    default:
+      return null
     }
   }
 }
