ee4a817b9d31004139884b24ef5dfcefa692f533
uncleseneca <uncleseneca@gmail.com>
2018-11-21 12:43:57 +0300

    fixed video playing in slides

--- features/slider/molecules/VideoPlayer/index.js
+++ features/slider/molecules/VideoPlayer/index.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactPlayer from 'react-player'
 import styles from './videoPlayer.css'
 import cx from 'classnames'
 
 const VideoPlayer = ({ src, height, width, className }) => {
   return (
-    <ReactPlayer
-      url={src}
+    <video
+      src={src}
       height={height}
       width={width}
       className={cx(styles.screen, className)}
-      playsinline={true}
-      autoPlay={true}
-      loop={true}
-      playing={true}
-      muted={true}
+      playsInline
+      autoPlay
+      loop
+      playing
+      muted
     />
   )
 }
@@ -23,7 +22,7 @@ const VideoPlayer = ({ src, height, width, className }) => {
 VideoPlayer.propTypes = {
   src: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
-  width: PropTypes.string.isRequired
+  width: PropTypes.string.isRequired,
 }
 
 export default VideoPlayer
