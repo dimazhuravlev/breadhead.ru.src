15d3cf43d11882dcc375af5c922979e15bb5e205
uncleseneca <uncleseneca@gmail.com>
2018-11-23 15:14:26 +0300

    fixed lint errors

--- features/home/page/Home/index.js
+++ features/home/page/Home/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Img from 'react-image'
 import Header from '@site/features/header'
 import Intro from '@site/features/intro'
 import Case from '@site/features/case'
