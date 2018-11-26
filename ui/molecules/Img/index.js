6754c825e609cc08cfa8990f7c0eac79d34f0bd8
uncleseneca <uncleseneca@gmail.com>
2018-11-26 10:04:28 +0300

    removed transition in Img component

--- ui/molecules/Img/index.js
+++ ui/molecules/Img/index.js
@@ -2,34 +2,17 @@ import React from 'react'
 import ReactImage from 'react-image'
 import cx from 'classnames'
 import styles from './Img.css'
-import { Transition, animated } from 'react-spring'
+import './ImgGlobal.css?CSSModulesDisable'
 
 const Img = ({ className, src, preloader }) => {
   return (
     <ReactImage
       className={className}
       src={[src]}
+      key={src}
       loader={
         <img className={cx(className, styles.preloader)} src={preloader} />
       }
-      container={children => {
-        return (
-          <Transition
-            native
-            items={React.Children.toArray(children)}
-            keys={child => child.props.src}
-            from={{ opacity: 0 }}
-            enter={{ opacity: 1 }}
-            leave={{ opacity: 0 }}
-          >
-            {child => props => (
-              <animated.div className={styles.wrapper} style={props}>
-                {child}
-              </animated.div>
-            )}
-          </Transition>
-        )
-      }}
     />
   )
 }
