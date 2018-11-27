38ab18021b68c727c0a1f4466cf09c332214b03e
uncleseneca <uncleseneca@gmail.com>
2018-11-27 11:43:09 +0300

    added google analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
@@ -5,6 +6,29 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(i, s, o, g, r, a, m) {
+                i["GoogleAnalyticsObject"] = r;
+                (i[r] =
+                  i[r] ||
+                  function() {
+                    (i[r].q = i[r].q || []).push(arguments);
+                  }),
+                  (i[r].l = 1 * new Date());
+                (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m);
+              })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+              ga("create", "UA-41499526-1", "breadhead.ru");
+              ga("send", "pageview");
+            `,
+            }}
+          />
+
           <link
             rel="apple-touch-icon"
             sizes="180x180"
