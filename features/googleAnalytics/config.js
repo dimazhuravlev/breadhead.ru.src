6d0cde1c3b0ab5498218d7e460fa0e4a7d95e773
uncleseneca <uncleseneca@gmail.com>
2018-11-27 11:54:25 +0300

    moved GA to feature

---
+++ features/googleAnalytics/config.js
@@ -0,0 +1,2 @@
+export const GA_ID = 'UA-41499526-1'
+export const DOMAIN_NAME = 'breadhead.ru'
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import Document, { Head, Main, NextScript } from 'next/document'
-
+import { getHTML } from '@site/features/googleAnalytics'
 export default class MyDocument extends Document {
   render() {
     return (
@@ -8,24 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-              (function(i, s, o, g, r, a, m) {
-                i["GoogleAnalyticsObject"] = r;
-                (i[r] =
-                  i[r] ||
-                  function() {
-                    (i[r].q = i[r].q || []).push(arguments);
-                  }),
-                  (i[r].l = 1 * new Date());
-                (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m);
-              })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
-
-              ga("create", "UA-41499526-1", "breadhead.ru");
-              ga("send", "pageview");
-            `,
+              __html: getHTML(),
             }}
           />
 
