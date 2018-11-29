b82fc23557fd7cafaae3a3eca6e3636ab5604c13
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-29 15:16:08 +0300

    add custom optimizating for mobile images

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,27 @@ const imagemin = require('gulp-imagemin')
 const rename = require('gulp-rename')
 const imageResize = require('gulp-image-resize')
 
-const targetImages = ['static/img/**/*.{jpg,png}', '!static/img/**/*-compress.{jpg,png}']
+const desktopImages = [
+  'static/img/**/*.{jpg,png}',
+  '!static/img/**/mobile/*.{jpg,png}',
+  '!static/img/**/*-compress.{jpg,png}',
+]
+const mobileImages = ['static/img/**/mobile/*.{jpg,png}', '!static/img/**/mobile/*-compress.{jpg,png}']
 
-gulp.task('images', () =>
-  gulp.src(targetImages)
+const DESKTOP_RESIZE_PERCENTAGE = 66
+const MOBILE_RESIZE_PERCENTAGE = 50
+
+const updateImagesHandler = (images, resizePercentage) => {
+  gulp.src(images)
     .pipe(imagemin())
-    .pipe(imageResize({ percentage: 66, imageMagick: true }))
+    .pipe(imageResize({ percentage: resizePercentage, imageMagick: true }))
     .pipe(rename({ suffix: '-compress' }))
     .pipe(gulp.dest('static/img/'))
-)
+}
+
+gulp.task('images', () => {
+  updateImagesHandler(desktopImages, DESKTOP_RESIZE_PERCENTAGE)
+  updateImagesHandler(mobileImages, MOBILE_RESIZE_PERCENTAGE)
+})
 
 gulp.task('default', ['images'])
