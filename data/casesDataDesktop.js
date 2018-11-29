eebf3c1c004d5da75559323b7572bff02304b64d
Igor Kamyshev <igor@kamyshev.me>
2018-11-29 15:32:55 +0300

    Modificate only for

--- data/casesDataDesktop.js
+++ data/casesDataDesktop.js
@@ -21,7 +21,7 @@ export const casesDataDesktop = [
         }
       },
       {
-        onlyFor: 'ru',
+        onlyFor: ['ru'],
         type: TemplatesNames.ArticleWithPhoneDesktop,
         duration: 12000,
         data: {
