a1e2f657d76f91cd45934a760afae715a3ff954a
dimazhuravlev <dmitryzhur@gmail.com>
2018-12-03 15:27:34 +0300

    added language versions to meta

--- pages/_app.js
+++ pages/_app.js
@@ -24,9 +24,37 @@ class MyApp extends App {
       <Container>
         <Head>
           <title>{t('title')}</title>
+
+          <meta name="title" content={t('title')} />
+          <meta name="description" content={t('meta-description')} />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://breadhead.ru/" />
+          <meta property="og:title" content={t('title')} />
+          <meta property="og:description" content={t('meta-description')} />
+          <meta
+            property="og:image"
+            content="https://breadhead.ru/static/img/bh_title.jpg"
+          />
+          <meta property="og:image:width" content="1080" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:locale" content={t('meta-locale')} />
+          <meta property="og:site_name" content="breadhead.ru" />
+          <meta property="fb:app_id" content="306467899461953" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://breadhead.ru/static/img/bh_title.jpg"
+          />
+          <meta property="twitter:title" content={t('title')} />
           <meta
             property="twitter:description"
-            content={t('title')}
+            content={t('meta-description')}
+          />
+          <meta
+            property="twitter:image"
+            content="https://breadhead.ru/static/img/bh_title.jpg"
           />
         </Head>
         <Sprite />
@@ -38,5 +66,5 @@ class MyApp extends App {
 
 export default compose(
   appWithTranslation,
-  withNamespaces(['common']),
+  withNamespaces(['common'])
 )(MyApp)
