82f65c4145124c8c58e681c6746584fa6e9d0f40
leonid <leon@thatsme.ru>
2018-12-05 21:46:14 +0300

    experiment with font preload

--- pages/_document.js
+++ pages/_document.js
@@ -9,18 +9,20 @@ export default class MyDocument extends Document {
     return (
       <html lang={i18n.language}>
         <Head>
+          
+          <link rel="preload" href="/static/fonts/Aeroport-light.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+          <link rel="preload" href="/static/fonts/Graphik-Light-Cy-Web.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+              
           <script
             dangerouslySetInnerHTML={{
               __html: getHTML()
             }}
           />
-
           <style
             dangerouslySetInnerHTML={{
               __html: getCSS()
             }}
           />
-          
           <link
             rel="apple-touch-icon"
             sizes="180x180"
