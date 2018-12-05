7fd3f759ba7347690dfcee08d3918c08b40d4a15
dimazhuravlev <dmitryzhur@gmail.com>
2018-12-05 15:53:12 +0300

    fixed button height

--- ui/atoms/icons/index.js
+++ ui/atoms/icons/index.js
@@ -1,3 +1,4 @@
 export { default as BreadheadLogo } from './BreadheadLogo'
 export { default as ZipIcon } from './ZipIcon'
 export { default as PlusIcon } from './PlusIcon'
+export { default as LessIcon } from './LessIcon'
