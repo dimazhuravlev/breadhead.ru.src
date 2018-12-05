4033b22a6a58939c22835f4b10fd44c49517a9cf
dimazhuravlev <dmitryzhur@gmail.com>
2018-12-05 17:34:32 +0300

    removed LessIcon

--- ui/atoms/icons/index.js
+++ ui/atoms/icons/index.js
@@ -1,4 +1,3 @@
 export { default as BreadheadLogo } from './BreadheadLogo'
 export { default as ZipIcon } from './ZipIcon'
 export { default as PlusIcon } from './PlusIcon'
-export { default as LessIcon } from './LessIcon'
