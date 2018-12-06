7da79d048f55820ef0b9c65bcb6ad7fc06f6f156
leonid <leon@thatsme.ru>
2018-12-06 14:06:38 +0300

    I don't want to fetch to much in preloader

--- ui/atoms/Image/index.js
+++ ui/atoms/Image/index.js
@@ -4,10 +4,13 @@ import { requireImage, requireImageCompressed, requireWebp, requireWebpCompresse
 const srcSetWebp = (path) => `${requireWebpCompressed(path)} 1x, ${requireWebp(path)} 2x`
 const srcSetJpeg = (path) => `${requireImageCompressed(path)} 1x, ${requireImage(path)} 2x`
 
-const Image = ({ src, className, ...rest }) => (
+const Image = ({ src, className, preloader = false, ...rest }) => (
   <picture className={className}>
-    <source srcSet={srcSetWebp(src)} type="image/webp" className={className} {...rest} />
-    <source srcSet={srcSetJpeg(src)} className={className} type="image/jpeg" {...rest} />
+    {!preloader && (<>
+        <source srcSet={srcSetWebp(src)} className={className} type="image/webp"  {...rest} />
+        <source srcSet={srcSetJpeg(src)} className={className} type="image/jpeg" {...rest} />
+      </>)}
+    
 
     <img src={requireImage(src)} className={className} {...rest} />
   </picture>
