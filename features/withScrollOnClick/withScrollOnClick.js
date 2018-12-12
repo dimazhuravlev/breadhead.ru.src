91930277c2148cd6df3e92a7c6a06c973a20e684
dimazhuravlev <dmitryzhur@gmail.com>
2018-12-12 12:38:30 +0300

    fix(layout): fix button transitions

--- features/withScrollOnClick/withScrollOnClick.js
+++ features/withScrollOnClick/withScrollOnClick.js
@@ -5,9 +5,9 @@ const withScrollOnClick = Component => {
   const ComponentWithScroll = props => {
     const scrollTo = props => {
       scroller.scrollTo(props.targetscroll, {
-        duration: 1600,
+        duration: 1400,
         delay: 0,
-        smooth: 'easeInOutQuint'
+        smooth: 'easeInOutCubic'
       })
     }
 
